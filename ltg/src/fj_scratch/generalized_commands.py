696e5331d58737279b688282946f3f078ae3e546
Fj <fj.mail@gmail.com>
2011-06-17 22:28:49 +0300

    generalized (get i) (get j)

---
+++ ltg/src/fj_scratch/generalized_commands.py
@@ -0,0 +1,41 @@
+from game import Game
+from rules import parse_commands, apply, card, Context, Function, Error
+from rules import IntValue
+from pprint import pprint
+ 
+class AbstractFunction(Function):
+    def __init__(self, name, required_type=None):
+        self.name = name
+        self.required_type = required_type
+    def apply(self, arg, context):
+        t = self.required_type
+        if t is not None and not isinstance(arg, t):
+            raise Error('wrong type')
+        return AbstractFunction('({0} {1})'.format(self.name, arg))
+    def __str__(self):
+        return self.name
+
+#game = Game(silent=False)
+
+def generate_get_i_get_j_command(i, j):
+    return ', '.join(['zero r'] + 
+                     ['succ l'] * i +
+                     ['get l', 'K l', 'S l', 'get r'] +
+                     ['K l', 'S l', 'succ r'] *j + 
+                     ['zero r'])
+s = generate_get_i_get_j_command(3, 7)
+commands = parse_commands(s)
+pprint(commands)
+
+context = Context(None)
+
+state = card.I
+
+for cmd, side in commands:
+    if cmd == card.get: 
+        cmd = AbstractFunction('get', IntValue) 
+    if side == 'r':
+        state = apply(state, cmd, context)
+    else:
+        state = apply(cmd, state, context)
+    print cmd, side, ':', state
