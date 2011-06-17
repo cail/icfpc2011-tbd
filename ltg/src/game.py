4ccdfef310907d2f018651cfa260fa864412b437
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2011-06-17 11:24:21 +0400

    zombie phase

--- ltg/src/game.py
+++ ltg/src/game.py
@@ -35,8 +35,12 @@ class Player(object):
     def __init__(self):
         self.vitalities = [INITIAL_VITALITY]*SLOTS
         self.values = [Identity.instance]*SLOTS
+    def has_alive_slots(self):
+        return any(v > 0 for v in self.vitalities)
     def num_alive_slots(self):
-        return sum(1 for v in self.vitalities if v > 0)    
+        return sum(1 for v in self.vitalities if v > 0)
+    def has_zombies(self):
+        return -1 in self.vitalities    
 
     
 class Game(object):
@@ -45,14 +49,40 @@ class Game(object):
         self.players = [Player(), Player()]
         self.proponent, self.opponent = self.players
         self.half_moves = 0
+
+        #self.proponent.vitalities[13] = -1
+        #self.proponent.values[13] = zero
         
     def is_finished(self):
         return \
             self.half_moves >= MAX_TURNS*2 or \
-            any(p.num_alive_slots() == 0 for p in self.players)
-        
+            any(not p.has_alive_slots() for p in self.players)
+            
+    def zombie_phase(self):
+        prop = self.proponent
+        if not self.silent:
+            print 'zombie phase'
+        for i in range(SLOTS):
+            if prop.vitalities[i] == -1:
+                if not self.silent:
+                    print 'zombie in slot', i, 'is applied to I'
+                z = prop.values[i]
+                context = Context(self, zombie=True)
+                try:
+                    result = apply(z, Identity.instance, context)
+                except Error as e:
+                    if not self.silent:
+                        print e
+                prop.values[i] = Identity.instance
+                prop.vitalities[i] = 0
+                if not self.silent:
+                    print 'zombie is rested'
+            
     def make_half_move(self, direction, slot, card):
-        #TODO: zombies
+        if self.proponent.has_zombies():
+            self.zombie_phase()
+        # TODO: make GameTrackingBot see state after zombie phase, not before
+            
         self.apply(
             slot, 
             card_by_name[card], 
