703c0b946dc1fb56dde6b5d1ab4e4b687f08c423
Pavel Lepin <pbl64k@gmail.com>
2011-06-17 03:05:25 -0700

    Abstraction elimination.

--- ltg/src/abselim.py
+++ ltg/src/abselim.py
@@ -1,6 +1,9 @@
 
 import re
 
+class LambdaParserException(Exception):
+    pass
+
 def m(string):
     strlen = len(string)
     if strlen == 0:
@@ -12,7 +15,7 @@ def m(string):
 
 def elim_ws(string):
     s = m(string)
-    if re.search(r'^\s$', s[0]) != None:
+    if re.match(r'\s', s[0]) != None:
         return elim_ws(s[1])
     else:
         return string
@@ -30,28 +33,28 @@ def parse_expr(expr, string):
     s = m(elim_ws(string))
     if s[0] == ')' or s[1] == '':
         if expr == None:
-            raise Exception('null expression')
+            raise LambdaParserException('null expression')
         return (expr, s[1])
     elif s[0] == '(':
         interm = parse(s[1])
         return parse_expr(join_expr(expr, interm[0]), interm[1])
-    elif expr == None and re.search(r'^\\$', s[0]):
+    elif expr == None and re.match(r'\\', s[0]):
         return parse_lambda(s[1])
-    elif re.search(r'^[A-Za-z]$', s[0]):
+    elif re.match(r'[A-Za-z]', s[0]):
         return parse_expr(join_expr(expr, Atom(s[0])), s[1])
     else:
-        raise Exception('unrecognized symbol')
+        raise LambdaParserException('unrecognized symbol')
 
 def parse_lambda(string):
     s = m(elim_ws(string))
-    if not re.search(r'^[a-z]$', s[0]):
-        raise Exception('bad lambda')
+    if not re.match(r'[a-z]', s[0]):
+        raise LambdaParserException('bad lambda')
     return parse_lambda2(Atom(s[0]), s[1])
 
 def parse_lambda2(variable, string):
     s = m(elim_ws(string))
-    if not re.search(r'^\.$', s[0]):
-        raise Exception('malformed lambda')
+    if not re.match(r'\.', s[0]):
+        raise LambdaParserException('malformed lambda')
     interm = parse(s[1])
     return (Lambda(variable, interm[0]), interm[1])
 
@@ -87,12 +90,22 @@ class Lambda:
     def dump(self):
         return '(\\' + self.variable.dump() + '. ' + self.expr.dump() + ')'
     def transform(self):
-        return self
+        xexpr = self.expr.transform()
+        if not xexpr.free(self.variable):
+            return Expr(Atom('K'), xexpr)
+        elif xexpr.equals(self.variable):
+            return Atom('I')
+        elif ((not xexpr.left.free(self.variable)) and
+              xexpr.right.equals(self.variable)):
+            return xexpr.left
+        else:
+            return Expr(Expr(Atom('S'), Lambda(self.variable, xexpr.left).transform()),
+                        Lambda(self.variable, xexpr.right).transform())
     def free(self, atom):
         return (not self.variable.equals(atom)) and self.expr.free(atom)
     def equals(self, atom):
         return False
 
 if __name__ == '__main__':
-    print parse(r'(\x. (\y. y x))')[0].dump()
+    print parse(r'(\x. (\y. y x))')[0].transform().dump()
 
