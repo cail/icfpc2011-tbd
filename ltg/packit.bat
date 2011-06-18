d241700171860f64318e4ed07e773d0341a07fcf
Fj <fj.mail@gmail.com>
2011-06-19 00:16:14 +0300

    use git archive for creating submission; edit .gitattributes to exclude more files

--- ltg/packit.bat
+++ ltg/packit.bat
@@ -1,5 +1,1 @@
-@echo off
-
-flip -u install run src/*
-tar -czf --exclude *.pyc tbd.tar.gz install run src
-flip -m install run src/*
+git archive -v --format=tar -o submission.tar HEAD src install run
