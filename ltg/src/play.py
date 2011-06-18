4818b31621116afbed2aa2996109cecfa2415e87
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2011-06-18 12:57:24 +0400

    replay generator

--- ltg/src/play.py
+++ ltg/src/play.py
@@ -6,8 +6,10 @@ from bot import IdleBot, RandomBot, InteractiveBot
 if __name__ == '__main__':
     game = Game(silent=False)
     
-    bots = [InteractiveBot(game), IdleBot(game)]
-    replay = []
+    replay = open('replay.txt', 'w')
+    
+    bots = [RandomBot(game), IdleBot(game)]
+
     while not game.is_finished():
         print>>sys.stderr, 'half turn', game.half_moves
         if game.has_zombie_phase():
@@ -15,7 +17,7 @@ if __name__ == '__main__':
         move = bots[game.half_moves%2].choose_move()
         print>>sys.stderr, move[0], move[1], move[2]
         game.make_half_move(*move)
-        replay.append(move)
+        print>>replay, move[0], move[1], move[2]
 
     
     print 'game ended at half turn', game.half_moves
