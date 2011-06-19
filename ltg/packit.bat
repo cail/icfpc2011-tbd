a1d8e8a14b87996fdd48c4cddd11e8f9458bb040
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2011-06-19 16:31:30 +0400

    reminder about -OO flag

--- ltg/packit.bat
+++ ltg/packit.bat
@@ -1,1 +1,2 @@
+echo add -OO flag to final submission! (but not to test ones)
 git -c core.eol=lf -c core.autocrlf=false archive -v --format=tar -o submission.tar HEAD src install run && gzip -f submission.tar
--- ltg/packit.sh
+++ ltg/packit.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+echo add -OO flag to final submission! (but not to test ones)
 git archive -v --format=tar HEAD src install run | gzip >submission.tar.gz
