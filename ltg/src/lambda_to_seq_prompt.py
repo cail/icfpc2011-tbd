ed393882b1a38c6fcac591074de2ae28ee7a9eea
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2011-06-20 12:37:16 +0400

    interactive lambda to sequence converter

---
+++ ltg/src/lambda_to_seq_prompt.py
@@ -0,0 +1,27 @@
+from traceback import print_exc
+import sys
+
+from terms import lambda_to_sequence, sequence_to_str
+from abselim import LambdaParserException
+
+
+
+if __name__ == '__main__':
+    while True:
+        print '>>>',
+        s = raw_input()
+        try:
+            seq = lambda_to_sequence(s)
+        except LambdaParserException as e:
+            print e
+            continue
+        except Exception as e:
+            sys.stdout.flush()
+            print_exc()
+            continue
+        print len(seq),
+        seq = sequence_to_str(seq)
+        if len(seq) > 70:
+            seq = seq[:70]+' ...'
+        print seq
+        
